447b3d6bd369cc3a211ab4d0dd751b6a932bb285
Omkar Kulkarni <o.s.kulkarni2004@gmail.com>
2024-08-29 19:09:12 +0530

    Add package-lock.json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import fs from "graceful-fs";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
